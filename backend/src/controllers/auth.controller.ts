3f51f9071e0e5e270f0befcb7d82638dceab9081
Kurasao <jamerolenor@gmail.com>
2025-12-13 17:22:55 +0800

    refactor(auth): comment out OTP generation and email sending logic in signup and login functions

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -3,7 +3,7 @@ import User from "../models/user.model";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { AuthRequest } from "../middlewares/auth.middleware";
-import { generateOTP, sendOTPEmail } from "../utils/email.services";
+// import { generateOTP, sendOTPEmail } from "../utils/email.services";
 
 export async function signup(req: Request, res: Response) {
   try {
@@ -21,19 +21,18 @@ export async function signup(req: Request, res: Response) {
     if (existing) return res.status(400).json({ message: "Email already exists" });
 
     const passwordHash = await bcrypt.hash(password, 10);
-    const otp = generateOTP();
-    const otpExpire = new Date(Date.now() + 10 * 60 * 1000);
+    // const otp = generateOTP();
+    // const otpExpire = new Date(Date.now() + 10 * 60 * 1000);
 
-    sendOTPEmail(email, otp).catch((err) => {
-      console.log("Failed to send OTP email", err);
-    });
+    // sendOTPEmail(email, otp)
+    // .catch((err) => console.log("Failed to send OTP email", err));
 
     await User.create({
       name,
       email,
       passwordHash,
-      otp,
-      otpExpire
+      // otp,
+      // otpExpire
     });
 
     return res.status(201).json({ message: "User registered. Please verify OTP sent to email" });
@@ -42,65 +41,65 @@ export async function signup(req: Request, res: Response) {
   }
 };
 
-export async function verifyOTP(req: Request, res: Response) {
-  try {
-    const { email, otp } = req.body as {
-      email: string,
-      otp: string
-    };
+// export async function verifyOTP(req: Request, res: Response) {
+//   try {
+//     const { email, otp } = req.body as {
+//       email: string,
+//       otp: string
+//     };
 
-    const user = await User.findOne({ email });
-    if (!user) return res.status(404).json({ message: "User not found" });
-    if (user.isVerified) return res.status(400).json({ message: "User already verified" });
-    if (user.otp !== otp || user.otpExpire < new Date()) {
-      return res.status(400).json({ message: "OTP is invalid or expired" });
-    }
+//     const user = await User.findOne({ email });
+//     if (!user) return res.status(404).json({ message: "User not found" });
+//     if (user.isVerified) return res.status(400).json({ message: "User already verified" });
+//     if (user.otp !== otp || user.otpExpire < new Date()) {
+//       return res.status(400).json({ message: "OTP is invalid or expired" });
+//     }
 
-    user.isVerified = true;
-    user.otp = "";
-    user.otpExpire = new Date(Date.now());
-    await user.save();
+//     user.isVerified = true;
+//     user.otp = "";
+//     user.otpExpire = new Date(Date.now());
+//     await user.save();
 
-    const token = jwt.sign(
-      { userId: String(user._id) },
-      process.env['JWT_SECRET_KEY'] as string,
-      { expiresIn: "24h" },
-    );
+//     const token = jwt.sign(
+//       { userId: String(user._id) },
+//       process.env['JWT_SECRET_KEY'] as string,
+//       { expiresIn: "24h" },
+//     );
 
-    res.cookie("accessToken", token, {
-      httpOnly: true,
-      secure: true,
-      sameSite: "none",
-      maxAge: 1000 * 60 * 60 * 24,
-    });
+//     res.cookie("accessToken", token, {
+//       httpOnly: true,
+//       secure: true,
+//       sameSite: "none",
+//       maxAge: 1000 * 60 * 60 * 24,
+//     });
 
-    return res.status(200).json({ message: "Email verified successfully" });
-  } catch (error) {
-    return res.status(500).json({ message: "Error verifying OTP" });
-  }
-};
+//     return res.status(200).json({ message: "Email verified successfully" });
+//   } catch (error) {
+//     return res.status(500).json({ message: "Error verifying OTP" });
+//   }
+// };
 
-export async function resendOTP(req: Request, res: Response) {
-  try {
-    const { email } = req.body as { email: string };
+// export async function resendOTP(req: Request, res: Response) {
+//   try {
+//     const { email } = req.body as { email: string };
 
-    const user = await User.findOne({ email });
-    if (!user) return res.status(404).json({ message: "User not found" });
-    if (user.isVerified) return res.status(400).json({ message: "User already verified" });
+//     const user = await User.findOne({ email });
+//     if (!user) return res.status(404).json({ message: "User not found" });
+//     if (user.isVerified) return res.status(400).json({ message: "User already verified" });
 
-    const otp = generateOTP();
-    user.otp = otp;
-    user.otpExpire = new Date(Date.now() + 10 * 60 * 1000);
-    await user.save();
+//     const otp = generateOTP();
+//     user.otp = otp;
+//     user.otpExpire = new Date(Date.now() + 10 * 60 * 1000);
+//     await user.save();
 
-    sendOTPEmail(email, otp, "Resend OTP verification")
-      .catch((err) => console.error("Failed to send OTP:", err));;
+//     sendOTPEmail(email, otp, "Resend OTP verification")
+//       .catch((err) => console.error("Failed to send OTP:", err));
 
-    return res.status(200).json({ message: "OTP resent successfully" });
-  } catch (error) {
-    return res.status(500).json({ message: "Error resending OTP" });
-  }
-};
+//     return res.status(200).json({ message: "OTP resent successfully" });
+//   } catch (error) {
+//     return res.status(500).json({ message: "Error resending OTP" });
+//   }
+// };
 
 export async function login(req: Request, res: Response) {
   try {
@@ -117,17 +116,17 @@ export async function login(req: Request, res: Response) {
     const ok = await bcrypt.compare(password, user.passwordHash);
     if (!ok) return res.status(401).json({ message: "Invalid credentials" });
 
-    if (!user.isVerified) {
-      const otp = generateOTP();
-      user.otp = otp;
-      user.otpExpire = new Date(Date.now() + 10 * 60 * 1000);
-      await user.save();
+    // if (!user.isVerified) {
+    //   const otp = generateOTP();
+    //   user.otp = otp;
+    //   user.otpExpire = new Date(Date.now() + 10 * 60 * 1000);
+    //   await user.save();
 
-      sendOTPEmail(email, otp, "Resend OTP verification")
-        .catch((err) => console.error("Failed to send OTP:", err));;
+    //   sendOTPEmail(email, otp, "Resend OTP verification")
+    //     .catch((err) => console.error("Failed to send OTP:", err));
 
-      return res.status(403).json({ message: "Verify email first. OTP sent to email" });
-    };
+    //   return res.status(403).json({ message: "Verify email first. OTP sent to email" });
+    // };
 
     const token = jwt.sign(
       { userId: String(user._id) },
@@ -174,4 +173,4 @@ export async function getMe(req: AuthRequest, res: Response) {
   });
 };
 
-export default { signup, verifyOTP, resendOTP, login, logout, getMe };
+export default { signup, login, logout, getMe };
